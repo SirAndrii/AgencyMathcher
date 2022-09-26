602a5bb9851a9b5d9186283dafe680f0f2027a62
sirAndrii <andrii@aclipp.com>
2022-09-26 16:33:04 +0200

    added server.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     reactStrictMode: false,
     images: {
+        loader: 'akamai', //optimization engine for images
+        path: '/',
         domains: ['www.adobe.com',"cdn.dribbble.com","i.pinimg.com"],
     },
+    distDir: '.next',
 }
